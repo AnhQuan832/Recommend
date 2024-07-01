5c8586edabaeda4547af259261b5e0afbb28fee7
Anh Quan <quandang123654@gmail.com>
2024-07-01 14:01:08 +0700

    change similar option to pearson

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import requests
 import logging
-from surprise import Dataset, Reader, KNNBasic, SVD
+from surprise import Dataset, Reader, KNNBasic, SVD, accuracy
 from surprise.model_selection import train_test_split
 import joblib
 import os
@@ -16,11 +16,11 @@ app = Flask(__name__)
 api = Api(app)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
-api_url='https://kltn-pescue-production.up.railway.app/api/v1/'
+api_url='https://kltn-pescue-production.up.railway.app/api/v1/data/'
 api_header = {'client-id' : 'PqescSU7WscLlNRvHK2Ew397vBa0b7dr','client-key': 'opIGrWw2u0WBmZHVIyDRqM6t0P2NKE1c'}
 #Item-based Collaborative Filtering
 sim_options = {
-    'name': 'cosine',
+    'name': 'pearson',
     'user_based': False  
 }
 
@@ -100,43 +100,54 @@ def generate_random_data(num_users=100, num_products=50, num_invoices=500):
 
 def get_recommend_product(product_id,  num_recommendations=5):
     try:
-        product_raw = pd.DataFrame(get_products())
+        rating_raw = pd.DataFrame(get_products())
         viewed_product_raw = pd.DataFrame(get_view_by_product(product_id))
         invoice_data = pd.DataFrame(get_invoices())
-        viewed_product_raw = viewed_product_raw.dropna()
-        if viewed_product_raw.empty or invoice_data.empty or product_raw.empty:
-            app.logger.info("Empty data", viewed_product_raw.empty , invoice_data.empty , product_raw.empty)
+        # viewed_product_raw = viewed_product_raw.dropna()
+        if viewed_product_raw.empty or invoice_data.empty or rating_raw.empty:
+            app.logger.info("Empty data", viewed_product_raw.empty , invoice_data.empty , rating_raw.empty)
             return []
         
         viewed_product_raw = viewed_product_raw.rename(columns={'viewerId': 'userId', 'objectId': 'productId'})
         user_id_map = {id: idx + 1 for idx, id in enumerate(invoice_data['userId'].unique())}
-        product_id_map = {id: idx + 1 for idx, id in enumerate(product_raw['productId'].unique())}
+        product_id_map = {id: idx + 1 for idx, id in enumerate(rating_raw['productId'].unique())}
 
         invoice_data.loc[:, 'userId_mapped'] = invoice_data['userId'].map(user_id_map)
         invoice_data.loc[:, 'productId_mapped'] = invoice_data['productId'].map(product_id_map)
         viewed_product_raw.loc[:, 'userId_mapped'] = viewed_product_raw['userId'].map(user_id_map)
         viewed_product_raw.loc[:, 'productId_mapped'] = viewed_product_raw['productId'].map(product_id_map)
-        product_raw.loc[:, 'productId_mapped'] = product_raw['productId'].map(product_id_map)
+        rating_raw.loc[:, 'productId_mapped'] = rating_raw['productId'].map(product_id_map)
+        rating_raw.loc[:, 'userId_mapped'] = rating_raw['userId'].map(user_id_map)
 
-        product = product_raw[['productId_mapped', 'categoryName', 'price']]
+
         viewed_product = viewed_product_raw[['userId_mapped']]
-        invoice = invoice_data[['userId_mapped', 'productId_mapped']]
-       
-
+        rating = rating_raw[['userId_mapped', 'productId_mapped','score']]
         #change the userId and productId to mapped values
        
 
         product_id_mapped = product_id_map[product_id]
         viewed_product = viewed_product.dropna(subset=['userId_mapped'])
         users_who_viewed = set(viewed_product['userId_mapped'])
-        relevant_invoices = invoice[invoice['userId_mapped'].isin(users_who_viewed)]
+        app.logger.info(rating);
+        app.logger.info(users_who_viewed);
 
-        reader = Reader(rating_scale=(1, 1))  
-        data = Dataset.load_from_df(relevant_invoices[['userId_mapped', 'productId_mapped', 'userId_mapped']], reader)
+        relevant_invoices = rating[rating['userId_mapped'].isin(users_who_viewed)]
+        app.logger.info(relevant_invoices);
+        reader = Reader(rating_scale=(1, 5))  
+        data = Dataset.load_from_df(relevant_invoices[['userId_mapped', 'productId_mapped', 'score']], reader)
 
         trainset, testset = train_test_split(data, test_size=0.1)
         model = load_or_train_model(trainset, MODEL_PATH_REC)
         model.fit(trainset)
+        predictions = model.test(testset)
+        sim_matrix = model.compute_similarities()
+        app.logger.info(sim_matrix)
+
+
+        y_true = [pred.r_ui for pred in predictions]
+        y_pred = [pred.est for pred in predictions]
+        print(f'y_true: {y_true[:10]}')
+        print(f'y_pred: {y_pred[:10]}')
         inner_id = model.trainset.to_inner_iid(product_id_mapped)
         neighbors = model.get_neighbors(inner_id, k=num_recommendations)
 
@@ -144,13 +155,9 @@ def get_recommend_product(product_id,  num_recommendations=5):
 
         # neighbors = [list(product_id_map.keys())[list(product_id_map.values()).index(inner_id)] for inner_id in neighbors]
 
-        app.logger.info(product_raw)
-        # app.logger.info(pd.DataFrame(neighbors, columns=['productId_mapped']))
+        similar_products = pd.DataFrame(neighbors, columns=['productId_mapped']).merge(rating_raw, left_on='productId_mapped', right_on='productId_mapped')
 
-        similar_products = pd.DataFrame(neighbors, columns=['productId_mapped']).merge(product_raw, left_on='productId_mapped', right_on='productId_mapped')
-
-        app.logger.info(similar_products.head())
-        similar_products = similar_products.rename(columns={'viewerId': 'userId', 'objectId': 'productId'})
+        similar_products = similar_products.drop_duplicates(subset=['productId'])
         similar_products_json = similar_products['productId'].tolist()
         response = {'listProduct': similar_products_json}
         return response
@@ -159,39 +166,30 @@ def get_recommend_product(product_id,  num_recommendations=5):
 
 def recommend_similar_products(product_id, num_recommendations=5):
     product_data = pd.DataFrame(get_products())
-    # Tạo product_id_map
     product_id_map = {id: idx + 1 for idx, id in enumerate(product_data['productId'].unique())}
     
-    # Ánh xạ productId sang productId_mapped
     product_data.loc[:, 'productId_mapped'] = product_data['productId'].map(product_id_map)
     
-    # Loại bỏ các giá trị null nếu có
     product_data.dropna(subset=['productId_mapped', 'avgRating'], inplace=True)
     
-    # Tạo đối tượng Reader và Dataset
     reader = Reader(rating_scale=(product_data['avgRating'].min(), product_data['avgRating'].max()))
-    data = Dataset.load_from_df(product_data[['productId_mapped', 'productId_mapped', 'avgRating']], reader)
+    data = Dataset.load_from_df(product_data[['productId_mapped', 'productId_mapped', 'score']], reader)
     
-    # Chia tập dữ liệu thành tập huấn luyện và tập kiểm tra
     trainset, testset = train_test_split(data, test_size=0.2)
     
     model = load_or_train_model(trainset, MODEL_PATH_SIM)
     model.fit(trainset)
     
-    # Kiểm tra nếu product_id tồn tại trong product_id_map
     if product_id not in product_id_map:
         raise ValueError(f"Product ID {product_id} is not part of the trainset")
     
     # Lấy inner_id của product_id
     inner_id = model.trainset.to_inner_iid(product_id_map[product_id])
     
-    # Lấy các hàng xóm gần nhất
     neighbors = model.get_neighbors(inner_id, k=num_recommendations)
     
-    # Chuyển đổi inner_id sang raw_id
     neighbors = [model.trainset.to_raw_iid(inner_id) for inner_id in neighbors]
     
-    # Tạo DataFrame từ danh sách các productId được gợi ý
     neighbors_mapped = [list(product_id_map.keys())[list(product_id_map.values()).index(n)] for n in neighbors]
     similar_products = pd.DataFrame(neighbors_mapped, columns=['productId']).merge(product_data, on='productId')
     similar_products_json = similar_products['productId'].tolist()
@@ -201,27 +199,27 @@ def recommend_similar_products(product_id, num_recommendations=5):
 def load_or_train_model(trainset, model_path):
     if os.path.exists(model_path):
         model = joblib.load(model_path)
+        app.logger.info("Model loaded")
     else:
         model = KNNBasic(sim_options=sim_options)
         model.fit(trainset)
-        # Save model
         joblib.dump(model, model_path)
     return model
 
 
 def get_products():
     try:
-        response = requests.get(f'{api_url}product')
+        response = requests.get(f'{api_url}rating',headers=api_header)
         if response.status_code == 200:
             res = response.json()
             if res['meta']['statusCode'] == '0_2_s':
-                return res['data']['productList']
+                return res['data']['ratingData']
     except Exception as e:
         return {"message": "Error: " + str(e)}
 
 def get_view_by_product(product_id):
     try:
-        response = requests.get(f'{api_url}data/views-audit-log/{product_id}', headers=api_header)
+        response = requests.get(f'{api_url}views-audit-log/{product_id}', headers=api_header)
         if response.status_code == 200:
             res = response.json()
             if res['meta']['statusCode'] == '0_2_s':
@@ -231,7 +229,7 @@ def get_view_by_product(product_id):
     
 def get_invoices():
     try:
-        response = requests.get(f'{api_url}data/invoice-data', headers=api_header)
+        response = requests.get(f'{api_url}invoice', headers=api_header)
         if response.status_code == 200:
             res = response.json()
             if res['meta']['statusCode'] == '0_2_s':
